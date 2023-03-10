ae7719bacc8a0db4f7ee51d0ffa98a3c959595c5
injoy <qqq@www.com>
2023-03-10 17:52:22 +0300

    init img for simulation_lan_ReadMe

--- test.py
+++ test.py
@@ -4,11 +4,9 @@ for i in range(10):
 	dict_[str(i)] = i
 
 # dict_['7'] = 8
-last_v = 0
+last_v = -1
 
 for k, v in dict_.items():
-	if last_v == 0:
-		continue
 	if v != last_v + 1:
 		print('Alarm !!!!', k, v)
 	last_v = v
