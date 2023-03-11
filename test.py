0aaf69b3cc6bc9b28d81bc970ecec06ac75c6e3e
injoy <qqq@www.com>
2023-03-11 03:15:23 +0300

    :))

--- test.py
+++ test.py
@@ -1,9 +1,8 @@
 
-lst = [-1, 0, 1, 2, 3]
+text = "Lorem Ipsum is simply dummy text"
+len_text = len(text)
+max_len = 12
 
-res = all([i == lst[lst.index(i) + 1] + 1 for i in lst[:-1]])
-res = [i == lst[lst.index(i) + 1] + 1 for i in lst[:-1]]
+res = text if len_text <= max_len else text[:max_len] + '...'
+
 print(res)
-
-# print(lst[lst.index(-1)])
-print(lst[lst.index(-1) + 1])
