f90019dc0feab36395bb24089c9fa7d277f6b507
injoy <qqq@www.com>
2023-03-13 00:51:04 +0300

    rgb to hex 2

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 
-def rgb_to_hex(rgb):
+def rgb_to_hex(r, g, b):
+	lst = [r, g, b]
 	res = ''
-	lst = rgb.split(', ')
 	for el in lst:
 		h = hex(int(el)).upper()
 		res += h[2:] if len(h) == 4 else '0' + h[2:]
 	print(res)
 
 
-rgb_to_hex('14, 15, 128')
+rgb_to_hex(14, 15, 255)
 
