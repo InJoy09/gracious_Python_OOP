19c9d91270aaa5f0160953b4bf54d64e5c721520
injoy <qqq@www.com>
2023-03-13 01:18:03 +0300

    333

--- test.py
+++ test.py
@@ -1,12 +1,8 @@
 
-def rgb_to_hex(r, g, b):
-	lst = [r, g, b]
-	res = ''
-	for el in lst:
-		h = hex(int(el)).upper()
-		res += h[2:] if len(h) == 4 else '0' + h[2:]
-	print(res)
-
-
-rgb_to_hex(14, 15, 255)
+s = 'recede'
+res = ''
+for ch in s:
+	res += '(' if s.count(ch) == 1 else ')'
+	
+print(res)
 
