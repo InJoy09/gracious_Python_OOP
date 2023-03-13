f9aff28fb262e8bb435ab647ab2ae672773f38fe
injoy <qqq@www.com>
2023-03-13 21:08:54 +0300

    667 - 2

--- test.py
+++ test.py
@@ -2,6 +2,9 @@
 n = 70304
 n_lst = list(str(n))
 print(n_lst)
+res = ''
 for i in range(len(n_lst)):
 	el = n_lst[i]
-	print(el + ('0' * len(n_lst[i + 1:])))
+	if el != '0':
+		res += el + ('0' * len(n_lst[i + 1:])) + ' + '
+print(res[:-2])
